16eeb77be3361bc9dbd2c5d25cd89737f558348d
mayoumelon <mayoumelon@gmail.com>
2022-02-05 14:02:08 +0900

    add Vue3

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1><slot></slot></h1>
+  <p> I'm an example component.</p>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
